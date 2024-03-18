6e55f35052a9a1fcd332ca3e74d1d88afba458c1
SW-Muriu <sw.muriu@gmail.com>
2024-03-18 16:10:33 +0300

    Completed the megaBytes Converter

--- src/Keywords.java
+++ src/Keywords.java
@@ -1,5 +1,6 @@
 public class Keywords {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
---
+++ src/megBytesConverter.java
@@ -0,0 +1,14 @@
+public class megBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(-50);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        System.out.println(((kiloBytes)<0)? "Invalid Value" : (kiloBytes +
+                " KB = " +
+                ((kiloBytes - (kiloBytes%1024))/1024) +
+                " MB and " + (kiloBytes%1024) + " KB"));
+    }
+}
+
+
