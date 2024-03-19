7d46f180d458b7f4bc904a063ef5bfd87bca9498
SW-Muriu <sw.muriu@gmail.com>
2024-03-19 10:53:57 +0300

    Added the average calculator

---
+++ src/DecimalComparator.java
@@ -0,0 +1,18 @@
+//Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+//The method should return boolean and it needs to return true
+// if two double numbers are the same up to three decimal places. Otherwise, return false.
+
+public class DecimalComparator {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double firstDecimalNumber, double secondDecimalNumber) {
+        boolean simmilariy = true;
+
+
+        return simmilariy;
+    }
+
+
+}
