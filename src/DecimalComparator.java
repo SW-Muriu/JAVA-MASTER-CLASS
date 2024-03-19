da9b771805ec56e7817ac16cd73f444158b9a1b8
SW-Muriu <sw.muriu@gmail.com>
2024-03-19 13:03:57 +0300

    Completed the decimal Operator

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -4,15 +4,42 @@
 
 public class DecimalComparator {
     public static void main(String[] args) {
+        areEqualByThreeDecimalPlacesb(-3.1756,-3.175);
+        System.out.println("MyMethod:: ln");
+        areEqualByThreeDecimalPlaces(-3.1756,-3.175);
 
     }
 
+    //Truncates
     public static boolean areEqualByThreeDecimalPlaces(double firstDecimalNumber, double secondDecimalNumber) {
-        boolean simmilariy = true;
+        long multiplier = (long) Math.pow(10, 3);
+        double formattedFirstDecimalNumber = Math.floor(firstDecimalNumber * multiplier) / multiplier;
+        System.out.println("formattedFirstDecimalNumber: " + formattedFirstDecimalNumber);
+        double formattedSecondDecimalNumber = Math.floor(secondDecimalNumber * multiplier) / multiplier;
+        System.out.println("formattedSecondDecimalNumber: " + formattedSecondDecimalNumber);
 
 
-        return simmilariy;
+        System.out.println(formattedFirstDecimalNumber == formattedSecondDecimalNumber ? firstDecimalNumber + " and " + secondDecimalNumber + " are equal to the third decimal place" :
+                firstDecimalNumber + " and " + secondDecimalNumber + " are not equal to the third decimal place");
+
+        return formattedFirstDecimalNumber == formattedSecondDecimalNumber;
     }
 
+
+    //Rounds off
+    public static boolean areEqualByThreeDecimalPlacesb(double first, double second) {
+
+        long firstRounded = (long) (first * 1000);
+        System.out.println("firstRounded: " + firstRounded);
+        long secondRounded = (long) (second * 1000);
+        System.out.println("secondRounded: " + secondRounded);
+
+        System.out.println(  (firstRounded == secondRounded) ?
+                first + " and " + second + " are equal to the third decimal place" :
+                first + " and " + second + " are not equal to the third decimal place");
+
+        return firstRounded == secondRounded;
+    }
 
 }
+
