716239f3ccb7f3d2c6eddca9c6126d4b2f67b260
SW-Muriu <sw.muriu@gmail.com>
2024-03-20 15:30:36 +0300

    Adopted an overloaded method, in place of default declaration of values

--- src/MethodOverload/challenge0x01.java
+++ src/MethodOverload/challenge0x01.java
@@ -1,4 +1,20 @@
 package MethodOverload;
 
 public class challenge0x01 {
+    public static void main(String[] args) {
+
+        System.out.println("The inches are: " + convertToCentimetres(68));
+        System.out.println("The feet and inches are: " + convertToCentimetres(5, 8));
+
+    }
+
+    //Converts inches to centimetres
+    public static double convertToCentimetres(int inches) {
+        return inches * 2.54;
+    }
+
+    //Converts feet and inches to inches, then calls the first method to convert the inches to centimeters
+    public static double convertToCentimetres(int feet, int inches) {
+        return convertToCentimetres((feet * 12) + inches);
+    }
 }
