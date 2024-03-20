3ea893dcc8166df9fd19ca0754e4c3944d1ff234
SW-Muriu <sw.muriu@gmail.com>
2024-03-20 10:07:00 +0300

    Created a teen number checker

---
+++ src/CommonParamsInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CommonParamsInput {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] variablesInput(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input the first number: ");
+        int x = in.nextInt();
+        System.out.println("Input the second number: ");
+        int y = in.nextInt();
+        System.out.println("Input the thurd number: ");
+        int z = in.nextInt();
+
+        int [] inputIntegers = {x,y,z};
+
+        return inputIntegers;
+
+    }
+}
---
+++ src/TeenNumberChecker.java
@@ -0,0 +1,44 @@
+//We'll say that a number is "teen" if it is in the range 13 -19 (inclusive).
+//Write a method named hasTeen with 3 parameters of type int.
+//The method should return boolean and it needs to return true if one
+// of the parameters is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
+//Write another method named isTeen with 1 parameter of type int.
+//The method should return boolean and it needs to return true if the parameter is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
+
+
+public class TeenNumberChecker {
+    public static void main(String[] args) {
+        CommonParamsInput commonParamsInput = new CommonParamsInput();
+        int [] inputIntegers = commonParamsInput.variablesInput();
+
+        hasTeen(inputIntegers[0],inputIntegers[1], inputIntegers[2]);
+        isTeen(13);
+
+    }
+
+
+    public static boolean hasTeen(int x, int y, int z) {
+        boolean isWithinRange = false;
+        int[] numberArray = {x, y, z};
+        for (int number : numberArray) {
+            System.out.println(number >= 13 && number <= 19 ? "The number: " + number + " is within the range 13 - 19" :
+                    "The number: " + number + " is outside the range 13 -19");
+            System.out.println("The answer is true");
+            isWithinRange = number >= 13 && number <= 19? true: false;
+
+        }
+        return isWithinRange;
+
+    }
+
+    public static boolean isTeen (int a){
+        boolean isTeen = false;
+        System.out.println(a >= 13 && a <=19 ? "The number: " + a + " is a teen number" :
+                "The number: " + a + " is not a teen number");
+        isTeen = a >= 13 && a <= 19 ? true : false;
+
+        return isTeen;
+    }
+
+
+}
