aeed3df65ce6821e187157b0ea356902aec941de
SW-Muriu <sw.muriu@gmail.com>
2024-06-12 08:46:13 +0300

    Using break and continue together

--- src/SEC02ControlFlow/Loops/WhileLoop/EvenNumberChecker.java
+++ src/SEC02ControlFlow/Loops/WhileLoop/EvenNumberChecker.java
@@ -3,13 +3,20 @@ package SEC02ControlFlow.Loops.WhileLoop;
 public class EvenNumberChecker {
     public static void main(String[] args) {
 
-        int i = 5;
+        int i = 4;
+        int evenNumberCounter = 0;
+        int oddNumberCounter = 0;
         while (i <= 20){
             i++;
-            if(!isEvenNumber(i)) continue;
-            System.out.println(i);
-
+            if(!isEvenNumber(i)) {
+                oddNumberCounter++;
+                continue;
+            }
+            evenNumberCounter++;
+            System.out.println("The even Numbers are: " + i + "; ");
+            if(evenNumberCounter == 5) break;
         }
+        System.out.println("The Total Count of Even numbers is: " + evenNumberCounter + " and that off odd numbers is: " + oddNumberCounter);
 
 
     }
