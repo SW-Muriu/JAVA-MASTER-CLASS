98e22acb4dcf6da6f6a442c6e59d7c0ae67382ad
SW-Muriu <sw.muriu@gmail.com>
2024-06-12 14:43:42 +0300

    Adding all the even digits in  a number

---
+++ src/SEC02ControlFlow/Loops/Exercises/EvenDigitSum.java
@@ -0,0 +1,35 @@
+package SEC02ControlFlow.Loops.Exercises;
+
+public class EvenDigitSum {
+
+    public static void main(String[] args) {
+
+        int testNumber = 43784;
+        int sum = getEvenDigitSum(testNumber);
+        System.out.println("The sum of even digits in " + testNumber + " is: " + sum);
+
+    }
+
+    public static int getEvenDigitSum(int number){
+
+        int sumOfEvenDigits = 0;
+
+        //Return negative one to mark invalidity
+        if(number < 0 ) return -1;
+
+        int lastDigit = number % 10;
+
+        if (lastDigit % 2 == 0 ) sumOfEvenDigits += lastDigit;
+
+        while (number >= 10){
+            number /= 10;
+            int newLastDigit = number % 10;
+
+            if (newLastDigit % 2 == 0 ) sumOfEvenDigits += newLastDigit;
+        }
+
+        return sumOfEvenDigits;
+
+    }
+}
+
