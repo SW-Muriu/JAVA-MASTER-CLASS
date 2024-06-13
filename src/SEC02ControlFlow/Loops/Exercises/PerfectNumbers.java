cc066a8ae6a35035b96fa243dedc191314d3564b
SW-Muriu <sw.muriu@gmail.com>
2024-06-13 15:02:36 +0300

    Computation of perfect numbers

---
+++ src/SEC02ControlFlow/Loops/Exercises/PerfectNumbers.java
@@ -0,0 +1,24 @@
+package SEC02ControlFlow.Loops.Exercises;
+
+public class PerfectNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(10));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        //Return false for negative numbers
+        if (number <= 1) return false;
+
+        int factor = 1;
+        int sumOfProperFactors = 0;
+        while (factor < number) {
+            if (number % factor == 0) {
+                sumOfProperFactors += factor;
+            }
+            factor++;
+        }
+        return sumOfProperFactors == number;
+    }
+
+}
