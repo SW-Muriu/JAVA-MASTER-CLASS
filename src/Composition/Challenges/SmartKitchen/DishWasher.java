6085c17df6e5c6e2afcd0d6e3c2dec7c3078f069
SW-Muriu <sw.muriu@gmail.com>
2024-07-25 13:18:13 +0300

    The Smart Kitchen

---
+++ src/Composition/Challenges/SmartKitchen/DishWasher.java
@@ -0,0 +1,16 @@
+package Composition.Challenges.SmartKitchen;
+
+public class DishWasher {
+    private boolean hasWorkToDo;
+
+    public void doDishes() {
+        if (hasWorkToDo) {
+            System.out.println("Dish washer is washing dishes");
+            this.hasWorkToDo = false;
+        }
+    }
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+}
