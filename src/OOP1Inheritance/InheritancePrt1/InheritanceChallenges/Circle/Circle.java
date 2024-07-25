77b08a1f7582d12e10081ee5f1eda66691eff9e6
SW-Muriu <sw.muriu@gmail.com>
2024-07-25 09:57:59 +0300

    Completed the inheritance challenges

---
+++ src/OOP1Inheritance/InheritancePrt1/InheritanceChallenges/Circle/Circle.java
@@ -0,0 +1,18 @@
+package OOP1Inheritance.InheritancePrt1.InheritanceChallenges.Circle;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = (radius < 0) ? 0: radius;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+}
