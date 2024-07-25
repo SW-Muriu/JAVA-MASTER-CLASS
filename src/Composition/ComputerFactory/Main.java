84cc5f0280ee34f8b56d2313a4e0682f144104f0
SW-Muriu <sw.muriu@gmail.com>
2024-07-25 12:04:46 +0300

    Inheritance vs Composition

---
+++ src/Composition/ComputerFactory/Main.java
@@ -0,0 +1,18 @@
+package Composition.ComputerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        ComputerCase theComputerCase = new ComputerCase("2208", "Dell");
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, "2540 * 1440");
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PersonalComputer thePC = new PersonalComputer("2208", "Dell", theComputerCase, theMonitor, theMotherboard);
+
+
+//        thePC.getMonitor().drawPixelArt(3, 5, "Blue");
+//        thePC.getMotherboard().loadProgram("Windows");
+//        thePC.getComputerCase().pressPowerButton();
+
+        thePC.powerUp();
+    }
+}
---
+++ src/Composition/README.md
@@ -0,0 +1,7 @@
+//Composition
+1. Inheritance defines an IS-A relationship
+2. Composition defines a HAS-A relationship
+3. Inheritance is a way to reuse functionality and attributes
+4. Composition is a way to make the combination of classes act like a single coherent object
+5. As a general rule, when creating a program, try using composition, before implementing inheritance
+3. 
