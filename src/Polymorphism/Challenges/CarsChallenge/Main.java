bdab2def9397b42401ec819f4387964ea4e4c078
SW-Muriu <sw.muriu@gmail.com>
2024-08-01 23:42:15 +0300

    Enhanced polymorphism assignment

--- src/Polymorphism/Challenges/CarsChallenge/Main.java
+++ src/Polymorphism/Challenges/CarsChallenge/Main.java
@@ -8,9 +8,18 @@ public class Main {
         Car theFerrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS",
                 15.4, 6);
         runRace(theFerrari);
+
+        Car tesla = new ElectricCar("2022 Red Tesla Model 3",
+                6, 75);
+        runRace(tesla);
+
+        Car ferrariHybrid = new HybridCar("Black Ferrari SF90 Stradale",
+                16, 8, 8);
+        runRace(ferrariHybrid);
+
     }
 
-    public static void  runRace(Car car){
+    public static void runRace(Car car) {
         car.startEngine();
         car.drive();
     }
