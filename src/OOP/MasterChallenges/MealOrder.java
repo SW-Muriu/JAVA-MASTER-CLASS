68f847ebb3c2bfa366de970e2a61f54ca6a7b0ce
SW-Muriu <sw.muriu@gmail.com>
2024-08-05 12:40:34 +0300

    Bill's burger challenge

---
+++ src/OOP/MasterChallenges/MealOrder.java
@@ -0,0 +1,44 @@
+package OOP.MasterChallenges;
+
+public class MealOrder {
+
+    private Item side;
+    private Item drink;
+    private Burger burger;
+
+
+    public MealOrder() {
+        this("regular", "coke", "fries");
+    }
+
+    //Encapsulating the information about the Item and Burger
+    public MealOrder(String burgerType, String drinkType, String sideType) {
+        this.side = new Item(sideType, "side",1.50);
+        this.drink = new Item(drinkType, "drink", 1.0);
+        this.burger = new Burger(burgerType, 4.0);
+    }
+
+    public double getTotalPrice() {
+        return side.getAdjustedPrice() + drink.getAdjustedPrice() +
+                burger.getAdjustedPrice();
+    }
+
+    public void printItemizedList() {
+        burger.printItem();
+        side.printItem();
+        drink.printItem();
+        System.out.println("-".repeat(30));
+        Item.printItem("TOTAL PRICE", getTotalPrice());
+    }
+
+
+    public void addBurgerToppings(String extra1, String extra2, String extra3){
+        burger.addToppings(extra1,extra2,extra3);
+    }
+
+    public void setDrinkSize(String size){
+        drink.setSize(size);
+    }
+
+}
+
